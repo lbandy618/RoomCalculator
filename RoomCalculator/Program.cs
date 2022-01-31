dc538706de4bae857f723841d23afa082f399598
lbandy618 <97638884+lbandy618@users.noreply.github.com>
2022-01-31 15:13:45 -0500

    initial commit

---
+++ RoomCalculator/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // intro 
+            Console.WriteLine("Hello, this progam will help calculate the area and perimeter of various rectangular room.");
+            Console.WriteLine();
+            // getting info 
+            Console.WriteLine("Please enter the length of the room: ");
+            double length = double.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter the width of the room: ");
+            double width = double.Parse(Console.ReadLine());
+            Console.WriteLine();
+            // calculations
+            double area = length * width;
+            Console.WriteLine("The area of the room is " + area + ".");
+            double perimeter = (length * 2) + (width * 2);
+            Console.WriteLine("The perimeter of the room is " + perimeter + ".");
+
+
+        }
+    }
+}
