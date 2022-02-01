b141d10bfa28ce0d8a26de1b06abf075a93c535f
lbandy618 <97638884+lbandy618@users.noreply.github.com>
2022-02-01 09:27:00 -0500

    adding conditionals

--- RoomCalculator/Program.cs
+++ RoomCalculator/Program.cs
@@ -19,7 +19,7 @@ namespace MyApp // Note: actual namespace depends on the project name.
             double height = double.Parse(Console.ReadLine());
             Console.WriteLine();
 
-            // calculations
+            // calculations & display messages
             double area = length * width;
             Console.WriteLine("The area of the room is " + area + ".");
             double perimeter = (length * 2) + (width * 2);
@@ -28,6 +28,19 @@ namespace MyApp // Note: actual namespace depends on the project name.
             Console.WriteLine("The volume of the room is " + volume + ".");
             double surfaceArea = area * 5;
             Console.WriteLine("The surface area of the room is " + surfaceArea + ".");
+
+            if (area <= 250)
+            {
+                Console.WriteLine("This is a small sized room.");
+            }
+            else if(area > 250 && area < 650)
+            {
+                Console.WriteLine("This is a medium sized room.");
+            }
+            else if(area >= 650)
+            {
+                Console.WriteLine("This is a large sized room.");
+            }
 
         }
     }
